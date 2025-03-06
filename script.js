c9581402b750c8cd75d33be8e3639803ba2815b7
mhemon404 <mhemongaming@gmail.com>
2025-03-06 23:59:52 +0600

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // কাস্টমাইজ করার জন্য নিচের লিংকগুলো পরিবর্তন করুন
-const videoID = "I2bvoO0CoDQ";  // ইউটিউব ভিডিও ID দিন
+const videoID = "SGf1ZByL4Ec";  // ইউটিউব ভিডিও ID দিন
 const downloadLink = "https://github.com/mhemon404/project01/releases/download/Panel/Sportzfy_TV_v5.0.apk"; // সফটওয়্যার ডাউনলোড লিংক দিন
 
 // ভিডিও সেট করা
